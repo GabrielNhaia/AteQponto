fb68ed0ccd82d0766b36a8c274d4281524496682
Jhonatan Felippe <68928497+Jhowslow@users.noreply.github.com>
2022-04-08 22:04:23 -0300

    up

--- App.js
+++ App.js
@@ -1,106 +1,128 @@
+//DESENVOLVIDO PELA EQUIPE MAPA DO CRIME - TCC(USC)
+import React, { Component } from 'react';
+import { StatusBar, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Tela1 from './Telas/Tela1';
-import { Component } from 'react';
-import { Button, Modal, StyleSheet, Text, View, } from 'react-native';
+import signup from './telas/signup';
+import login from './telas/login';
+import telaCentral from './telas/telaCentral';
 
-export default class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {isVisible:false};
-  }
-  render ( ) {
-
+class App extends Component {
+  render() {
     const Stack = createNativeStackNavigator();
-    
-    return(
-      
 
-      /*
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Tela1'>
-          <Stack.Screen
-            name="Tela1" Component={Tela1}
-            opitions={{
-              title: "teste",
-            }} />
-        </Stack.Navigator>
-        </NavigationContainer>
+    return (
         <NavigationContainer>
-          <Stack.Navigator initialRouteName='Tela1'>
+          <Stack.Navigator initialRouteName='login'>
             <Stack.Screen
-              name="Tela1" Component={Tela1}
-            >
-            </Stack.Screen>
+              name='login'
+              component={login}
+            />
+            <Stack.Screen
+              name='signup'
+              component={signup}
+            />
+            <Stack.Screen
+              name='telaCentral'
+              component={telaCentral}
+            />            
           </Stack.Navigator>
         </NavigationContainer>
-        
-        */
-      
-
-      <View style={styles.container}>
-
-          <Modal style={styles.modal}
-            animationType={'slide'}
-            transparent={false}
-            visible={this.state.isVisible}
-            onRequestClose={() => {
-              this.setState({ isVisible: false });
-            } }
-          >
-            <Text>Conteudo do modal</Text>
-
-            <Button
-              title="tela2"
-              onPress={() => {
-                this.setState({ isVisible: false });
-              } } />
-
-            <Button
-              title="teste"
-              onPress={() => {
-                this.setState({ isVisible: false });
-              } } />
-          </Modal>
-
-
-          <Text>Aperte o botão para abrir o modal</Text>
-          <Button
-            title="teste"
-            onPress={() => {
-              this.setState({ isVisible: true });
-            } } />
-
-
-
-        </View>
-      
+     
     );
   }
-
 }
+
+/*
+const App = () => {
+
+  const Stack = createNativeStackNavigator();
+  return (
+
+    <View style={styles.container}>
+      <NavigationContainer>
+
+        <Stack.Navigator initialRouteName='signup'>
+          <Stack.Screen
+            name='signup'
+            component={signup}
+          />
+          <Stack.Screen
+            name='App'
+            component={App}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <StatusBar
+        backgroundColor="#cbf2d6"
+        barStyle="light-content"
+      />
+      <Text style={styles.titulo}>AteQponto</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Usuário"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+      />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+          style={styles.userBtn}
+          NavigationContainer
+        >
+          <Text style={styles.btnTxt}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.userBtn}
+          onPress={() => this.props.navigation.navigate('signup')}
+        >
+          <Text style={styles.btnTxt}>Registrar-se</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+} */
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height:'30%',
-    position: 'absolute',
-    bottom: 0,    
-    backgroundColor: '#fff',
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#cbf2d6',
   },
-  modal:{
-    width: '50%',
-    height:'50%',    
-    position: 'relative',
-    bottom: 0,    
-    justifyContent: 'center',
+  titulo: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: '#0c0f0d',
   },
-  dialogo:{
-    bottom:0,
-    height:'50%', 
-    width:'50%',
+  input: {
+    width: '90%',
+    backgroundColor: '#fff',
+    padding: 15,
+    marginBottom: 10,
   },
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+
+  },
+  userBtn: {
+    backgroundColor: '#070a08',
+    padding: 15,
+    width: '45%',
+  },
+  btnTxt: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#fff',
+
+  }
 });
+
+export default App;
