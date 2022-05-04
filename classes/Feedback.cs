a8b94f6ffe1f19ef244da2c4bac1ccd2f9419dfe
Dirrard <diegocastilho6@gmail.com>
2022-05-03 21:40:32 -0300

    Classes
    A criação das três classes iniciais do sistema: Usuario, Ponto e Feedback

---
+++ classes/Feedback.cs
@@ -0,0 +1,10 @@
+public class Feedback
+{
+      public int ID {get; set;}
+      public Usuario Usuario {get; set;}
+      public Ponto Ponto {get; set;}
+      public DateTime Data {get; set;}
+      public string Comentario {get; set;}
+
+
+}
---
+++ classes/Usuario.cs
@@ -0,0 +1,9 @@
+public class Usuario
+{
+    public int ID {get; set;}
+    public string Nome {get; set;}
+    public string CPF {get; set;}
+    public string Email {get; set;}
+    public string Senha {get; set;}
+    public bool Notificacao {get; set;}
+}
