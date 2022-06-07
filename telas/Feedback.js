0f61b9018fd76cfe3c692415de6c21aec851e2f0
Bryan Felipe <bryanfelipebsi@hotmail.com>
2022-06-07 00:29:04 -0300

    Tela de Feedback
    Picker no Feedback - Data e Hora

--- telas/Feedback.js
+++ telas/Feedback.js
@@ -1,6 +1,14 @@
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import DatePicker from 'react-native-date-picker'
 
 export default function Feedback({ navigation }) {
+
+  const [horas, sethoras] = useState("09");
+  const [minutos, setminutos] = useState("30");
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
 
   return (
     <View style={styles.container}>
@@ -10,11 +18,79 @@ export default function Feedback({ navigation }) {
         </View>
         <View style={{ flex: 2, alignItems: 'flex-start', flexDirection: 'column' }}>
           <View>
-          <Text style={styles.ponto}>Ponto:</Text>
+            <Text style={styles.ponto}>Ponto:</Text>
           </View>
           <View style={styles.aoLado}>
             <Text style={styles.ponto}>Horario:</Text>
+            {/*Picker da hora*/}
+            <Picker
+              style={styles.picker}
+              selectedValue={horas}
+              onValueChange={(itemValue) =>
+                sethoras(itemValue)
+              }>
+              <Picker.Item label="01" value="01" />
+              <Picker.Item label="02" value="02" />
+              <Picker.Item label="03" value="03" />
+              <Picker.Item label="04" value="04" />
+              <Picker.Item label="05" value="05" />
+              <Picker.Item label="06" value="06" />
+              <Picker.Item label="07" value="07" />
+              <Picker.Item label="08" value="08" />
+              <Picker.Item label="09" value="09" />
+              <Picker.Item label="10" value="10" />
+              <Picker.Item label="11" value="11" />
+              <Picker.Item label="12" value="12" />
+              <Picker.Item label="13" value="13" />
+              <Picker.Item label="14" value="14" />
+              <Picker.Item label="15" value="15" />
+              <Picker.Item label="16" value="16" />
+              <Picker.Item label="17" value="17" />
+              <Picker.Item label="18" value="18" />
+              <Picker.Item label="19" value="19" />
+              <Picker.Item label="20" value="20" />
+              <Picker.Item label="21" value="21" />
+              <Picker.Item label="22" value="22" />
+              <Picker.Item label="23" value="23" />
+            </Picker>
+            {/*Picker dos minutos*/}
+            <Picker
+              style={styles.picker}
+              selectedValue={minutos}
+              onValueChange={(itemValue) =>
+                setminutos(itemValue)
+              }>
+              <Picker.Item label="00" value="00" />
+              <Picker.Item label="10" value="10" />
+              <Picker.Item label="20" value="20" />
+              <Picker.Item label="30" value="30" />
+              <Picker.Item label="40" value="40" />
+              <Picker.Item label="50" value="50" />
+            </Picker>
+          </View>
+          <View>
             <Text style={styles.ponto}>Data:</Text>
+
+            <TouchableOpacity
+              style={styles.userBtnOption}
+              onPress={() => setOpen(true)
+              }>
+              <Text style={styles.btnTxt}>Data</Text>
+            </TouchableOpacity>
+            {/*Picker data*/}
+            <DatePicker
+              modal
+              open={open}
+              date={date}
+              onConfirm={(date) => {
+                setOpen(false)
+                setDate(date)
+              }}
+              onCancel={() => {
+                setOpen(false)
+              }}
+            />
+
           </View>
           <View>
             <Text style={styles.ponto}>Acontecimento:</Text>
@@ -51,7 +127,7 @@ const styles = StyleSheet.create({
   tela: {
     height: '77%',
     width: '100%',
-    backgroundColor: '#f20',
+    backgroundColor: '#f02',
   },
   aoLado: {
     maxHeight: '10%',
@@ -59,6 +135,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
 
+  picker: {
+    width: "30%",
+    left: "3%",
+    color: "#fff"
+  },
   userBtnOption: {
     backgroundColor: '#070a08',
     padding: 15,
@@ -82,13 +163,13 @@ const styles = StyleSheet.create({
 
   titulo: {
     fontSize: 35,
-    color: '#fff',
+    color: '#ff0',
     textAlign: 'center',
   },
 
   ponto: {
     fontSize: 25,
-    color: '#fff',
+    color: '#ff0',
     marginLeft: 5,
   },
 }
