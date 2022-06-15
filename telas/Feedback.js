26952455350e18654465f69713a3880855c01608
Bryan Felipe <bryanfelipebsi@hotmail.com>
2022-06-15 15:44:23 -0300

    Tela de Feedback
    Listo

--- telas/Feedback.js
+++ telas/Feedback.js
@@ -1,9 +1,11 @@
-import { View, StyleSheet, TouchableOpacity, Text, Platform } from 'react-native';
+{/* Imports */ }
+import { View, StyleSheet, TouchableOpacity, Text, TextInput, Platform } from 'react-native';
 import React, { useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
 import DatePicker from 'react-native-date-picker'
 import IOSPicker from 'react-native-ios-picker';
 
+{/*Export*/ }
 export default function Feedback({ navigation }) {
 
   const dados = ["1", "2", "3", "4", "5", "6"];
@@ -15,17 +17,30 @@ export default function Feedback({ navigation }) {
 
   return (
     <View style={styles.container}>
+      {/* TAMANHO DA TELA */}
       <View style={styles.tela}>
+
+        {/* TITULO */}
         <View>
           <Text style={styles.titulo}>Feedback do Ponto</Text>
         </View>
-        <View style={{ flex: 2, alignItems: 'flex-start', flexDirection: 'column' }}>
+
+        {/* CONTEUDOS */}
+        <View style={styles.Conteudos}>
           <View>
             <Text style={styles.ponto}>Ponto:</Text>
+            <View style={styles.campoPonto}>
+              <TextInput
+                style={styles.inputPonto}
+                placeholder="Ponto"
+              />
+            </View>
+            <View style={{ width: "80%", marginLeft: 10 }}>
+            </View>
           </View>
           <View style={styles.aoLado}>
             <Text style={styles.ponto}>Horario:</Text>
-            {/*Picker da hora*/}
+            {/*PICKER DA HORA*/}
             <Picker
               style={styles.picker}
               selectedValue={horas}
@@ -56,7 +71,7 @@ export default function Feedback({ navigation }) {
               <Picker.Item label="22" value="22" />
               <Picker.Item label="23" value="23" />
             </Picker>
-            {/*Picker dos minutos*/}
+            {/*PPICKER DO MINUTOS*/}
             <Picker
               style={styles.picker}
               selectedValue={minutos}
@@ -71,18 +86,20 @@ export default function Feedback({ navigation }) {
               <Picker.Item label="50" value="50" />
             </Picker>
           </View>
-          <View>
+          <View style={{ flex: 2, flexDirection: 'row', }}>
             <Text style={styles.ponto}>Data:</Text>
+            <View style={{ width: "80%", marginLeft: 10 }}>
 
-            <TouchableOpacity
-              style={styles.userBtnOption}
-              onPress={() => setOpen(true)
-              }>
-              <Text style={styles.btnTxt}>Data</Text>
-            </TouchableOpacity>
+              {/* BOTÃO DE ABRIR O DATAPICKER */}
+              <TouchableOpacity
+                style={styles.userBtnOption}
+                onPress={() => setOpen(true)
+                }>
+                <Text style={styles.btnTxt}>Data</Text>
+              </TouchableOpacity>
+            </View>
 
-            {/*Picker data*/}
-
+            {/*PICKER DATA*/}
             <DatePicker
               modal
               open={open}
@@ -95,18 +112,24 @@ export default function Feedback({ navigation }) {
                 setOpen(false)
               }}
             />
-
           </View>
-          <View>
+          <View style={styles.terreno}>
             <Text style={styles.ponto}>Acontecimento:</Text>
             {/* <IOSPicker
               data={dados}
             onValueChange={(d, i) => this.change(d, i)} /> */}
-          </View>
+            <View style={styles.campo}>
 
+              <TextInput
+                style={styles.inputCampo}
+                placeholder="Feedback"
+              />
+            </View>
+          </View>
         </View>
       </View>
 
+      {/* BOTÕES ( Enviar FeedBack & Cancelar)  */}
       <View style={{ width: "100%", minHeight: '10%', alignItems: 'center', alignSelf: 'flex-end', justifyContent: 'center', }}>
         <TouchableOpacity
           style={styles.userBtnOption}
@@ -125,6 +148,9 @@ export default function Feedback({ navigation }) {
 
   );
 }
+
+{/* CSS DOS COMPONENTES ACIMA*/ }
+
 const styles = StyleSheet.create({
 
   container: {
@@ -137,13 +163,28 @@ const styles = StyleSheet.create({
     height: '77%',
     width: '100%',
     backgroundColor: '#2d742d',
+    alignItems: 'center'
+  },
+  Conteudos: {
+    flex: 2,
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    maxWidth: "93%",
+    backgroundColor: '#318c46',
+    borderRadius: 15,
   },
   aoLado: {
     maxHeight: '15%',
-    flex: 1,
+    flex: 2,
     flexDirection: 'row',
   },
-
+  terreno: {
+    minHeight: '40%',
+    minWidth: '95%',
+    flex: 2,
+    flexDirection: 'column',
+    marginLeft: '2%',
+  },
   picker: {
     width: "30%",
     color: "#fff",
@@ -171,17 +212,42 @@ const styles = StyleSheet.create({
 
   titulo: {
     fontSize: 35,
-    color: '#ff0',
+    color: '#fff',
     textAlign: 'center',
   },
 
   ponto: {
     fontSize: 25,
-    color: '#ff0',
+    color: '#fff',
     marginLeft: 5,
-    paddingTop: 30,
+    paddingTop: 20,
+  },
+  campo: {
+    minHeight: '100%',
+    minWidth: '95%',
+    flex: 2,
+    flexDirection: 'column',
+    paddingTop: 10,
+    borderRadius: 15,
   },
 
+  inputCampo: {
+    minHeight: '50%',
+    minWidth: '95%',
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    padding: 15,
+    marginBottom: 10,
+  },
+  inputPonto: {
+    minHeight: '5%',
+    minWidth: '75%',
+    borderRadius: 15,
+    backgroundColor: '#fff',
+    padding: 15,
+    marginTop: -35,
+    left: "28%",
+  },
 
 }
 );
--- telas/Notificacao.js
+++ telas/Notificacao.js
@@ -1,43 +1,72 @@
-import {View, StyleSheet,TouchableOpacity,Text} from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 
-export default function Notificacao({navigation}) {
+export default function Notificacao({ navigation }) {
 
-    return (
-        <View style={styles.container}>
-            <View style={{ width: "100%", minHeight: '10%', alignItems: 'center', }}>
-            <TouchableOpacity
-              style={styles.userBtnOption}
-              onPress={() => navigation.navigate('TelaCentral')
-              }>
-              <Text style={styles.btnTxt}>Sair🚪</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.titulo}>
+        <Text style={{ fontSize: 25 }}>Notificações❗</Text>
+      </View>
+      <View style={styles.conteudo}>
+        <Text style={{ fontSize: 20 }}>  Exemplo de conteudo</Text>
+      </View>
+      <View style={styles.conteudo}>
+      </View>
+      <View style={styles.conteudo}>
+      </View>
+      <View style={styles.conteudo}>
+      </View>
+      <View style={{ width: "100%", minHeight: '10%', alignItems: 'center', }}>
+        <TouchableOpacity
+          style={styles.userBtnOption}
+          onPress={() => navigation.navigate('TelaCentral')
+          }>
+          <Text style={styles.btnTxt}>Retornar🚪</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#2d742d',
-    },
-    userBtnOption: {
-      backgroundColor: '#070a08',
-      padding: 15,
-      borderRadius: 15,
-      width: '95%',
-      alignItems: 'center',
-    },
-    userBtn: {
-      backgroundColor: '#070a08',
-      padding: 15,
-      borderRadius: 15,
-      width: '35%',
-    },
-    btnTxt: {
-      fontSize: 14,
-      textAlign: 'center',
-      color: '#fff',
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: '#2d742d',
+  },
+  conteudo: {
+    backgroundColor: '#f00',
+    marginBottom: 20,
+    borderRadius: 20,
+    marginBottom: "2%",
+    width: "95%",
+    height: "18%",
+  },
+  titulo: {
+    backgroundColor: '#fff',
+    alignItems:'center',
+    borderRadius: 10,
+    marginBottom: "7%",
+    width: "75%",
+    height: "6%",
+  },
+  userBtnOption: {
+    backgroundColor: '#070a08',
+    padding: 15,
+    borderRadius: 15,
+    width: '95%',
+    alignItems: 'center',
+  },
+  userBtn: {
+    backgroundColor: '#070a08',
+    padding: 15,
+    borderRadius: 15,
+    width: '35%',
+  },
+  btnTxt: {
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#fff',
+  },
 });
